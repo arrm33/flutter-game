e62f9542ef3510e1969edbec827c3b6e7f4c3062
arrm33 <152839556+arrm33@users.noreply.github.com>
2025-05-04 15:22:47 +0300

    Delete src/lib/flappy_bird_game.dart
    flappy bird game delete

--- src/lib/flappy_bird_game.dart
+++
@@ -1,8 +0,0 @@
-import 'package:flame/game.dart';
-import 'package:flutter_game/components/background.dart';
-
-class FlappyBirdGame extends FlameGame{
-  Future<void> onLode() async{
-    add(Background());
-  }
-}
